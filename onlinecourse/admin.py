9c595929dc04ba6ddf1c9ca8297e8b8a6eabab17
sjp7work <sjp7work@gmail.com>
2022-06-13 00:45:08 +0000

    can submit tests and compare results

--- onlinecourse/admin.py
+++ onlinecourse/admin.py
@@ -25,6 +25,9 @@ class CourseAdmin(admin.ModelAdmin):
 class LessonAdmin(admin.ModelAdmin):
     list_display = ['title']
 
+class QuestionAdmin(admin.ModelAdmin):
+    inlines = [ChoiceInline]
+
 
 # <HINT> Register Question and Choice models here
 
@@ -32,5 +35,5 @@ admin.site.register(Course, CourseAdmin)
 admin.site.register(Lesson, LessonAdmin)
 admin.site.register(Instructor)
 admin.site.register(Learner)
-admin.site.register(Question)
+admin.site.register(Question, QuestionAdmin)
 admin.site.register(Choice)
--- onlinecourse/models.py
+++ onlinecourse/models.py
@@ -102,7 +102,7 @@ class Enrollment(models.Model):
     # Has question content
     # Other fields and methods you would like to design
 class Question(models.Model):
-    lesson = models.ForeignKey(Lesson, on_delete=models.CASCADE)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
     qustion_text = models.TextField()
     question_grade = models.IntegerField()
 
